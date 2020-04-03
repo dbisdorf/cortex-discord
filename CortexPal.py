e8197f1265c7aa0d4bda51706f467fda331ae972
don <dbisdorf@gmail.com>
2020-04-03 09:57:52 -0400

    More work for exception handling.

--- CortexPal.py
+++ CortexPal.py
@@ -10,14 +10,14 @@
 # Document synonyms in user help.
 # Move to an exception model of throwing errors.
 # Is there any point in using subcommands?
-# We can say "key in dict" and not "key in list(dict)"
 # Pinned message must be stored by server/channel as well
 # A straight array of CortexGames might not be the most efficient for sorting purposes
 # Program should work fine even if you haven't pinned a message
-# Auto-capitalizing?
+# Auto-capitalizing for names of pools/stress/complications/assets/etc?
 # Maybe the Cortex Game Information header is superfluous
 # Hero dice are a pool. Gonna need GroupedDicePools then.
 # Also do crisis and "growth" pools. Make "pool" a generic concept?
+# Comments!
 
 import discord
 import random
@@ -259,67 +259,6 @@ class CortexPal(commands.Cog):
             self.games.append([game_key, game_info])
         return game_info
 
-    """
-    def format_comp(self, key):
-        return 'D{0} {1}'.format(self.comp_pile[key], key)
-
-    def format_pp(self, key):
-        return '{0}: {1}'.format(key, self.pp_pile[key])
-
-    def format_stress(self, key):
-        output = '{0}: '.format(key)
-        stresses = self.stress_pile[key]
-        if not stresses:
-            output += 'none'
-        elif len(stresses) == 1 and list(stresses)[0] == UNTYPED_STRESS:
-            output += 'D{0}'.format(stresses[UNTYPED_STRESS])
-        else:
-            separator = ''
-            for stress in stresses:
-                output += '{0}{1} D{2}'.format(separator, stress, stresses[stress])
-                separator = ', '
-        return output
-
-    def format_doom(self):
-        output = ''
-        dice = sorted(list(doom_pool.keys()))
-        for die in dice:
-            if self.doom_pool[die] == 1:
-                output += 'D{0} '.format(die)
-            else:
-                output += '{0}D{1} '.format(self.doom_pool[die], die)
-        return output
-
-    def format_summary(self):
-        content = '**Cortex Game Information**\n'
-        if self.comp_pile:
-            content += '\n**Complications**\n'
-            for key in self.comp_pile:
-                content += self.format_comp(key) + '\n'
-        if self.pp_pile:
-            content += '\n**Plot Points**\n'
-            for key in self.pp_pile:
-                content += self.format_pp(key) + '\n'
-        if self.doom_pool:
-            content += '\n**Doom Pool**\n' + self.format_doom()
-        if self.stress_pile:
-            content += '\n**Stress**\n'
-            for key in self.stress_pile:
-                content += self.format_stress(key) + '\n'
-        return content
-
-    @commands.Cog.listener()
-    async def on_reaction_add(self, reaction, user):
-        if user != bot.user:
-            if reaction.emoji == '⬆️':
-                self.comp += 2
-                await reaction.message.remove_reaction('⬆️', user)
-            elif reaction.emoji == '⬇️':
-                self.comp -= 2
-                await reaction.message.remove_reaction('⬇️', user)
-            await reaction.message.edit(content=self.format_comps())
-    """
-
     @commands.command()
     async def info(self, ctx):
         game = self.get_game_info(ctx)
@@ -338,32 +277,36 @@ class CortexPal(commands.Cog):
     @commands.command()
     async def comp(self, ctx, *args):
         game = self.get_game_info(ctx)
+        output = ''
+        update_pin = False
         try:
             if not args:
-                await ctx.send('Use the `$comp` command like this:\n`$comp new 6 Cloud of Smoke` (creates a D6 Cloud of Smoke complication)\n`$comp stepback Dazed` (steps back the Dazed complication)')
+                output = 'Use the `$comp` command like this:\n`$comp new 6 Cloud of Smoke` (creates a D6 Cloud of Smoke complication)\n`$comp stepback Dazed` (steps back the Dazed complication)'
             elif args[0] == 'new':
                 find_die_error(args[1])
                 name = ' '.join(args[2:])
-                output = game.complications.add(name, int(args[1]))
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('New complication: ' + output)
+                output = 'New complication: ' + game.complications.add(name, int(args[1]))
+                update_pin = True
             elif args[0] == 'stepup':
                 name = ' '.join(args[1:])
-                output = game.complications.step_up(name)
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('Stepped up: ' + output)
+                output = 'Stepped up: ' + game.complications.step_up(name)
+                update_pin = True
             elif args[0] == 'stepback':
                 name = ' '.join(args[1:])
-                output = game.complications.step_back(name)
+                output = 'Stepped back: ' + game.complications.step_back(name)
+                update_pin = True
+            if update_pin:
                 await self.pinned_message.edit(content=game.output())
-                await ctx.send('Stepped back: ' + output)
+            await ctx.send(output)
         except CortexError as err:
             await ctx.send(err)
 
     @commands.command()
     async def pp(self, ctx, *args):
+        output = ''
+        update_pin = False
         if not args:
-            await ctx.send('Use the `$pp` command like this:\n`$pp give Alice 3` (gives Alice 3 PP)\n`$pp spend Alice` (spends one of Alice\'s PP)')
+            output = 'Use the `$pp` command like this:\n`$pp give Alice 3` (gives Alice 3 PP)\n`$pp spend Alice` (spends one of Alice\'s PP)'
         else:
             game = self.get_game_info(ctx)
             if len(args) > 2:
@@ -371,21 +314,21 @@ class CortexPal(commands.Cog):
             else:
                 qty = 1
             if args[0] == 'give':
-                output = game.plot_points.add(args[1], qty)
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('Plot points for ' + output)
+                output = 'Plot points for ' + game.plot_points.add(args[1], qty)
+                update_pin = True
             elif args[0] == 'spend':
-                output = game.plot_points.remove(args[1], qty)
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('Plot points for ' + output)
+                output = 'Plot points for ' + game.plot_points.remove(args[1], qty)
+                update_pin = True
+        if update_pin:
+            await self.pinned_message.edit(content=game.output())
+        await ctx.send(output)
 
     @commands.command()
     async def roll(self, ctx, *args):
-        error = None
         results = {}
         try:
             for arg in args:
-                error = find_die_error(arg)
+                find_die_error(arg)
                 if error:
                     break
                 die = int(arg)
@@ -406,92 +349,117 @@ class CortexPal(commands.Cog):
 
     @commands.command()
     async def doom(self, ctx, *args):
+        output = ''
+        update_pin = False
         game = self.get_game_info(ctx)
-        if not args:
-            await ctx.send('Use the `$doom` command like this:\n`$doom give 6 8` (gives the doom pool a D6 and D8)\n`$doom spend 10` (spends a D10 from the doom pool)')
-        elif args[0] == 'give':
-            for arg in args[1:]:
-                game.doom_pool.add(int(arg))
-            await self.pinned_message.edit(content=game.output())
-            await ctx.send('New doom pool: ' + game.doom_pool.output())
-        elif args[0] == 'spend':
-            for arg in args[1:]:
-                game.doom_pool.remove(int(arg))
-            await self.pinned_message.edit(content=game.output())
-            await ctx.send('New doom pool: ' + game.doom_pool.output())
+        try:
+            if not args:
+                output = 'Use the `$doom` command like this:\n`$doom give 6 8` (gives the doom pool a D6 and D8)\n`$doom spend 10` (spends a D10 from the doom pool)'
+            elif args[0] == 'give':
+                for arg in args[1:]:
+                    game.doom_pool.add(int(arg))
+                output = 'New doom pool: ' + game.doom_pool.output()
+                update_pin = True
+            elif args[0] == 'spend':
+                for arg in args[1:]:
+                    game.doom_pool.remove(int(arg))
+                output = 'New doom pool: ' + game.doom_pool.output()
+                update_pin = True
+            if update_pin:
+                await self.pinned_message.edit(content=game.output())
+            await ctx.send(output)
+        except CortexError as err:
+            await ctx.send(err)
 
     @commands.command()
     async def stress(self, ctx, *args):
-        if not args:
-            await ctx.send('use the `$stress` command like this:\n`$stress give Amy 8` (gives Amy D8 stress)\n`$stress give Ben Mental 6` (gives Ben D6 mental stress)\n`$stress stepup Cat Social` (steps up Cat\'s social stress)')
-        else:
-            game = self.get_game_info(ctx)
-            if args[0] == 'give':
-                if args[2].isdecimal():
-                    stress_name = UNTYPED_STRESS
-                    die = int(args[2])
-                else:
-                    stress_name = args[2]
-                    die = int(args[3])
-                output = game.stress.add(args[1], stress_name, die)
+        output = ''
+        update_pin = False
+        try:
+            if not args:
+                output = 'use the `$stress` command like this:\n`$stress give Amy 8` (gives Amy D8 stress)\n`$stress give Ben Mental 6` (gives Ben D6 mental stress)\n`$stress stepup Cat Social` (steps up Cat\'s social stress)'
+            else:
+                game = self.get_game_info(ctx)
+                if args[0] == 'give':
+                    if args[2].isdecimal():
+                        stress_name = UNTYPED_STRESS
+                        die = int(args[2])
+                    else:
+                        stress_name = args[2]
+                        die = int(args[3])
+                    output = 'Stress for ' + game.stress.add(args[1], stress_name, die)
+                    update_pin = True
+                elif args[0] == 'stepup':
+                    if len(args) == 2:
+                        stress_name = UNTYPED_STRESS
+                    else:
+                        stress_name = args[2]
+                    output = 'Stress for ' + game.stress.step_up(args[1], stress_name)
+                    update_pin = True
+                elif args[0] == 'stepback':
+                    if len(args) == 2:
+                        stress_name = UNTYPED_STRESS
+                    else:
+                        stress_name = args[2]
+                    output = 'Stress for ' + game.stress.step_back(args[1], stress_name)
+                    update_pin = True
+            if update_pin:
                 await self.pinned_message.edit(content=game.output())
-                await ctx.send('Stress for ' + output)
-            elif args[0] == 'stepup':
-                if len(args) == 2:
-                    stress_name = UNTYPED_STRESS
-                else:
-                    stress_name = args[2]
-                output = game.stress.step_up(args[1], stress_name)
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('Stress for ' + output)
-            elif args[0] == 'stepback':
-                if len(args) == 2:
-                    stress_name = UNTYPED_STRESS
-                else:
-                    stress_name = args[2]
-                output = game.stress.step_back(args[1], stress_name)
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('Stress for ' + output)
+            await ctx.send(output)
+        except CortexError as err:
+            await ctx.send(err)
 
     @commands.command()
     async def hero(self, ctx, *args):
-        if not args:
-            await ctx.send('Use the `$hero` command like this:\n`$hero give Alice 2` (gives Alice 2 hero dice)\n`$hero spend Alice` (spends one of Alice\'s hero dice)')
-        else:
-            game = self.get_game_info(ctx)
-            if len(args) > 2:
-                qty = int(args[2])
+        output = ''
+        update_pin = False
+        try:
+            if not args:
+                output = 'Use the `$hero` command like this:\n`$hero give Alice 2` (gives Alice 2 hero dice)\n`$hero spend Alice` (spends one of Alice\'s hero dice)'
             else:
-                qty = 1
-            if args[0] == 'give':
-                output = game.hero_dice.add(args[1], qty)
+                game = self.get_game_info(ctx)
+                if len(args) > 2:
+                    qty = int(args[2])
+                else:
+                    qty = 1
+                if args[0] == 'give':
+                    output = 'Hero dice for ' + game.hero_dice.add(args[1], qty)
+                    update_pin = True
+                elif args[0] == 'spend':
+                    output = 'Hero dice for ' + game.hero_dice.remove(args[1], qty)
+                    update_pin = True
+            if update_pin:
                 await self.pinned_message.edit(content=game.output())
-                await ctx.send('Hero dice for ' + output)
-            elif args[0] == 'spend':
-                output = game.hero_dice.remove(args[1], qty)
-                await self.pinned_message.edit(content=game.output())
-                await ctx.send('Hero dice for ' + output)
+            await ctx.send(output)
+        except CortexError as err:
+            await ctx.send(err)
 
     @commands.command()
     async def asset(self, ctx, *args):
-        game = self.get_game_info(ctx)
-        if not args:
-            await ctx.send('This is where we give syntax help for the command')
-        elif args[0] == 'new':
-            name = ' '.join(args[2:])
-            output = game.assets.add(name, int(args[1]))
-            await self.pinned_message.edit(content=game.output())
-            await ctx.send('New asset: ' + output)
-        elif args[0] == 'stepup':
-            name = ' '.join(args[1:])
-            output = game.assets.step_up(name)
-            await self.pinned_message.edit(content=game.output())
-            await ctx.send('Stepped up: ' + output)
-        elif args[0] == 'stepback':
-            name = ' '.join(args[1:])
-            output = game.assets.step_back(name)
-            await self.pinned_message.edit(content=game.output())
-            await ctx.send('Stepped back: ' + output)
+        output = ''
+        update_pin = False
+        try:
+            game = self.get_game_info(ctx)
+            if not args:
+                output = 'This is where we give syntax help for the command'
+            elif args[0] == 'new':
+                find_die_error(args[1])
+                name = ' '.join(args[2:])
+                output = 'New asset: ' + game.assets.add(name, int(args[1]))
+                update_pin = True
+            elif args[0] == 'stepup':
+                name = ' '.join(args[1:])
+                output = 'Stepped up ' + game.assets.step_up(name)
+                update_pin = True
+            elif args[0] == 'stepback':
+                name = ' '.join(args[1:])
+                output = 'Stepped back ' + game.assets.step_back(name)
+                update_pin = True
+            if update_pin:
+                await self.pinned_message.edit(content=game.output())
+            await ctx.send(output)
+        except CortexError as err:
+            await ctx.send(err)
 
 bot.add_cog(CortexPal(bot))
 bot.run(TOKEN)
