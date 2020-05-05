98ff7cb50b5cb05686c9239a1352f74b536b108b
don <dbisdorf@gmail.com>
2020-05-05 10:26:09 -0400

    More docstrings.

--- CortexPal.py
+++ CortexPal.py
@@ -96,6 +96,8 @@ bot = commands.Bot(command_prefix='$', description=ABOUT_TEXT)
 # Classes and functions follow.
 
 class CortexError(Exception):
+    """Exception class for command and rules errors specific to this bot."""
+
     def __init__(self, message, *args):
         self.message = message
         self.args = args
@@ -104,6 +106,8 @@ class CortexError(Exception):
         return self.message.format(*(self.args))
 
 def separate_dice_and_name(inputs):
+    """Sort the words of an input string, and identify which are dice notations and which are not."""
+
     dice = []
     words = []
     for input in inputs:
@@ -114,6 +118,8 @@ def separate_dice_and_name(inputs):
     return {'dice': dice, 'name': ' '.join(words)}
 
 def separate_numbers_and_name(inputs):
+    """Sort the words of an input string, and identify which are numerals and which are not."""
+
     numbers = []
     words = []
     for input in inputs:
@@ -124,6 +130,8 @@ def separate_numbers_and_name(inputs):
     return {'numbers': numbers, 'name': ' '.join(words)}
 
 def fetch_all_dice_for_parent(db_parent):
+    """Given an object from the database, get all the dice that belong to it."""
+
     dice = []
     cursor.execute('SELECT * FROM DIE WHERE PARENT_GUID=:PARENT_GUID', {'PARENT_GUID':db_parent.db_guid})
     fetching = True
@@ -138,6 +146,8 @@ def fetch_all_dice_for_parent(db_parent):
     return dice
 
 class Die:
+    """A single die, or a set of dice of the same size."""
+
     def __init__(self, expression=None, name=None, size=4, qty=1):
         self.name = name
         self.size = size
@@ -156,61 +166,81 @@ class Die:
                 self.size = int(numbers[1])
 
     def store_in_db(self, db_parent):
+        """Store this die in the database, under a given parent."""
+
         self.db_parent = db_parent
         self.db_guid = uuid.uuid1().hex
         cursor.execute('INSERT INTO DIE (GUID, NAME, SIZE, QTY, PARENT_GUID) VALUES (?, ?, ?, ?, ?)', (self.db_guid, self.name, self.size, self.qty, self.db_parent.db_guid))
         db.commit()
 
     def already_in_db(self, db_parent, db_guid):
+        """Inform the Die that it is already in the database, under a given parent and guid."""
+
         self.db_parent = db_parent
         self.db_guid = uuid.uuid1().hex
 
     def remove_from_db(self):
+        """Remove this Die from the database."""
+
         if self.db_guid:
             cursor.execute('DELETE FROM DIE WHERE GUID=:guid', {'guid':self.db_guid})
 
     def step_down(self):
+        """Step down the die size."""
+
         if self.size > 4:
             self.update_size(self.size - 2)
 
     def step_up(self):
+        """Step up the die size."""
+
         if self.size < 12:
             self.update_size(self.size + 2)
 
     def combine(self, other_die):
+        """Combine this die with another die (as when applying a new stress die to existing stress)."""
+
         if self.size < other_die.size:
             self.update_size(other_die.size)
         elif self.size < 12:
             self.update_size(self.size + 2)
 
     def update_size(self, new_size):
+        """Change the size of the die."""
+
         self.size = new_size
         if self.db_guid:
             cursor.execute('UPDATE DIE SET SIZE=:size WHERE GUID=:guid', {'size':self.size, 'guid':self.db_guid})
             db.commit()
 
     def update_qty(self, new_qty):
+        """Change the quantity of the dice."""
+
         self.qty = new_qty
         if self.db_guid:
             cursor.execute('UPDATE DIE SET QTY=:qty WHERE GUID=:guid', {'qty':self.qty, 'guid':self.db_guid})
 
     def is_max(self):
+        """Identify whether the Die is at the maximum allowed size."""
+
         return self.size == 12
 
     def output(self):
+        """Return the Die as a string suitable for output in Discord."""
+
         return str(self)
 
     def __str__(self):
+        """General purpose string representation of the Die."""
+
         if self.qty > 1:
             return '{0}D{1}'.format(self.qty, self.size)
         else:
             return 'D{0}'.format(self.size)
 
-"""
-NamedDice: a collection of user-named single-die traits.
-Suitable for complications and assets.
-"""
 class NamedDice:
+    """A collection of user-named single-die traits, suitable for complications and assets."""
+
     def __init__(self, category, group, db_parent, db_guid=None):
         self.dice = {}
         self.category = category
@@ -235,6 +265,8 @@ class NamedDice:
             self.dice[die.name] = die
 
     def remove_from_db(self):
+        """Remove these NamedDice from the database."""
+
         for name in list(self.dice):
             self.dice[name].remove_from_db()
         cursor.execute("DELETE FROM DICE_COLLECTION WHERE GUID=:db_guid", {'db_guid':self.db_guid})
@@ -242,9 +274,13 @@ class NamedDice:
         self.dice = {}
 
     def is_empty(self):
+        """Identify whether there are any dice in this object."""
+
         return not self.dice
 
     def add(self, name, die):
+        """Add a new die, with a given name."""
+
         die.name = name
         if not name in self.dice:
             die.store_in_db(self)
@@ -257,6 +293,8 @@ class NamedDice:
             return 'Raised to ' + self.output(name)
 
     def remove(self, name):
+        """Remove a die with a given name."""
+
         if not name in self.dice:
             raise CortexError(NOT_EXIST_ERROR, self.category)
         output = 'Removed: ' + self.output(name)
@@ -265,6 +303,8 @@ class NamedDice:
         return output
 
     def step_up(self, name):
+        """Step up the die with a given name."""
+
         if not name in self.dice:
             raise CortexError(NOT_EXIST_ERROR, self.category)
         if self.dice[name].is_max():
@@ -273,6 +313,8 @@ class NamedDice:
         return 'Stepped up to ' + self.output(name)
 
     def step_down(self, name):
+        """Step down the die with a given name."""
+
         if not name in self.dice:
             raise CortexError(NOT_EXIST_ERROR, self.category)
         if self.dice[name].size == 4:
@@ -283,12 +325,18 @@ class NamedDice:
             return 'Stepped down to ' + self.output(name)
 
     def get_all_names(self):
+        """Identify the names of all the dice in this object."""
+
         return list(self.dice)
 
     def output(self, name):
+        """For a die of a given name, return a formatted description of that die."""
+
         return '{0} {1}'.format(self.dice[name].output(), name)
 
     def output_all(self, separator='\n'):
+        """Return a formatted description of all the dice in this object."""
+
         output = ''
         prefix = ''
         for name in list(self.dice):
@@ -296,12 +344,9 @@ class NamedDice:
             prefix = separator
         return output
 
-"""
-DicePool: a single-purpose collection of die sizes and quantities.
-Suitable for doom pools, crisis pools, and growth pools.
-Not necessarily persisted in the database.
-"""
 class DicePool:
+    """A single-purpose collection of die sizes and quantities, suitable for doom pools, crisis pools, and growth pools."""
+
     def __init__(self, roller, group, incoming_dice=[]):
         self.roller = roller
         self.group = group
@@ -312,24 +357,34 @@ class DicePool:
             self.add(incoming_dice)
 
     def store_in_db(self, db_parent):
+        """Store this pool in the database."""
+
         self.db_guid = uuid.uuid1().hex
         self.db_parent = db_parent
         cursor.execute("INSERT INTO DICE_COLLECTION (GUID, CATEGORY, GRP, PARENT_GUID) VALUES (?, 'pool', ?, ?)", (self.db_guid, self.group, self.db_parent.db_guid))
         db.commit()
 
     def already_in_db(self, db_parent, db_guid):
+        """Inform the pool that it is already in the database, under a given parent and guid."""
+
         self.db_parent = db_parent
         self.db_guid = db_guid
 
     def fetch_dice_from_db(self):
+        """Get all the dice from the database that would belong to this pool."""
+
         fetched_dice = fetch_all_dice_for_parent(self)
         for die in fetched_dice:
             self.dice[DIE_SIZES.index(die.size)] = die
 
     def is_empty(self):
+        """Identify whether this pool is empty."""
+
         return not self.dice
 
     def remove_from_db(self):
+        """Remove this entire pool from the database."""
+
         for index in range(len(self.dice)):
             if self.dice[index]:
                 self.dice[index].remove_from_db()
@@ -338,6 +393,8 @@ class DicePool:
         self.dice = [None, None, None, None, None]
 
     def add(self, dice):
+        """Add dice to the pool."""
+
         for die in dice:
             index = DIE_SIZES.index(die.size)
             if self.dice[index]:
@@ -349,6 +406,8 @@ class DicePool:
         return self.output()
 
     def remove(self, dice):
+        """Remove dice from the pool."""
+
         for die in dice:
             index = DIE_SIZES.index(die.size)
             if self.dice[index]:
@@ -365,6 +424,8 @@ class DicePool:
         return self.output()
 
     def roll(self):
+        """Roll all the dice in the pool, and return a formatted summary of the results."""
+
         output = ''
         separator = ''
         for die in self.dice:
@@ -379,6 +440,8 @@ class DicePool:
         return output
 
     def output(self):
+        """Return a formatted list of the dice in this pool."""
+
         if self.is_empty():
             return 'empty'
         output = ''
@@ -388,6 +451,8 @@ class DicePool:
         return output
 
 class DicePools:
+    """A collection of DicePool objects."""
+
     def __init__(self, roller, db_parent):
         self.roller = roller
         self.pools = {}
@@ -405,14 +470,20 @@ class DicePools:
                 fetching = False
 
     def is_empty(self):
+        """Identify whether we have any pools."""
+
         return not self.pools
 
     def remove_from_db(self):
+        """Remove all of these pools from the database."""
+
         for group in list(self.pools):
             self.pools[group].remove_from_db()
         self.pools = {}
 
     def add(self, group, dice):
+        """Add some dice to a pool under a given name."""
+
         if not group in self.pools:
             self.pools[group] = DicePool(self.roller, group)
             self.pools[group].store_in_db(self.db_parent)
@@ -420,15 +491,21 @@ class DicePools:
         return '{0}: {1}'.format(group, self.pools[group].output())
 
     def remove(self, group, dice):
+        """Remove some dice from a pool with a given name."""
+
         if not group in self.pools:
             raise CortexError(NOT_EXIST_ERROR, 'pool')
         self.pools[group].remove(dice)
         return '{0}: {1}'.format(group, self.pools[group].output())
 
     def roll(self, group):
+        """Roll all the dice in a certain pool and return the results."""
+
         return self.pools[group].roll()
 
     def output(self):
+        """Return a formatted summary of all the pools in this object."""
+
         output = ''
         prefix = ''
         for key in list(self.pools):
@@ -437,6 +514,8 @@ class DicePools:
         return output
 
 class Resources:
+    """Holds simple quantity-based resources, like plot points."""
+
     def __init__(self, category, db_parent):
         self.resources = {}
         self.category = category
@@ -451,14 +530,20 @@ class Resources:
                 fetching = False
 
     def is_empty(self):
+        """Identify whether there are any resources stored here."""
+
         return not self.resources
 
     def remove_from_db(self):
+        """Removce these resources from the database."""
+
         cursor.executemany("DELETE FROM RESOURCE WHERE GUID=:db_guid", [{'db_guid':self.resources[resource]['db_guid']} for resource in list(self.resources)])
         db.commit()
         self.resources = {}
 
     def add(self, name, qty=1):
+        """Add a quantity of resources to a given name."""
+
         if not name in self.resources:
             db_guid = uuid.uuid1().hex
             self.resources[name] = {'qty':qty, 'db_guid':db_guid}
@@ -471,6 +556,8 @@ class Resources:
         return self.output(name)
 
     def remove(self, name, qty=1):
+        """Remove a quantity of resources from a given name."""
+
         if not name in self.resources:
             raise CortexError(HAS_NONE_ERROR, name, self.category)
         if self.resources[name]['qty'] < qty:
@@ -481,9 +568,13 @@ class Resources:
         return self.output(name)
 
     def output(self, name):
+        """Return a formatted description of the resources held by a given name."""
+
         return '{0}: {1}'.format(name, self.resources[name]['qty'])
 
     def output_all(self):
+        """Return a formatted summary of all resources."""
+
         output = ''
         prefix = ''
         for name in list(self.resources):
@@ -492,6 +583,8 @@ class Resources:
         return output
 
 class GroupedNamedDice:
+    """Holds named dice that are separated by groups, such as mental and physical stress (the dice names) assigned to characters (the dice groups)."""
+
     def __init__(self, category, db_parent):
         self.groups = {}
         self.category = category
@@ -510,42 +603,55 @@ class GroupedNamedDice:
             self.groups[group] = new_group
 
     def is_empty(self):
+        """Identifies whether we're holding any dice yet."""
+
         return not self.groups
 
     def remove_from_db(self):
+        """Remove all of these dice from the database."""
+
         for group in list(self.groups):
             self.groups[group].remove_from_db()
         self.groups = {}
 
     def add(self, group, name, die):
+        """Add dice with a given name to a given group."""
+
         if not group in self.groups:
             self.groups[group] = NamedDice(self.category, group, self.db_parent)
         return self.groups[group].add(name, die)
 
     def remove(self, group, name):
+        """Remove dice with a given name from a given group."""
+
         if not group in self.groups:
             raise CortexError(HAS_NONE_ERROR, group, self.category)
         return self.groups[group].remove(name)
 
     def step_up(self, group, name):
+        """Step up the die with a given name, within a given group."""
+
         if not group in self.groups:
             raise CortexError(HAS_NONE_ERROR, group, self.category)
         return self.groups[group].step_up(name)
 
     def step_down(self, group, name):
+        """Step down the die with a given name, within a given group."""
+
         if not group in self.groups:
             raise CortexError(HAS_NONE_ERROR, group, self.category)
         return self.groups[group].step_down(name)
 
-    def get_all_names(self):
-        return list(self.dice)
-
     def output(self, group):
+        """Return a formatted list of all the dice within a given group."""
+
         if self.groups[group].is_empty():
             return '{0}: None'.format(group)
         return '{0}: {1}'.format(group, self.groups[group].output_all(separator=', '))
 
     def output_all(self):
+        """Return a formatted summary of all dice under all groups."""
+        
         output = ''
         prefix = ''
         for group in list(self.groups):
@@ -554,6 +660,8 @@ class GroupedNamedDice:
         return output
 
 class CortexGame:
+    """All information for a game, within a single server and channel."""
+
     def __init__(self, roller, server, channel):
         self.roller = roller
         self.pinned_message = None
@@ -569,6 +677,8 @@ class CortexGame:
         self.new()
 
     def new(self):
+        """Set up new, empty traits for the game."""
+
         self.complications = NamedDice('complication', None, self)
         self.assets = NamedDice('asset', None, self)
         self.pools = DicePools(self.roller, self)
@@ -576,6 +686,8 @@ class CortexGame:
         self.stress = GroupedNamedDice('stress', self)
 
     def clean(self):
+        """Resets and erases the game's traits."""
+
         self.complications.remove_from_db()
         self.assets.remove_from_db()
         self.pools.remove_from_db()
@@ -583,6 +695,8 @@ class CortexGame:
         self.stress.remove_from_db()
 
     def output(self):
+        """Return a report of all of the game's traits."""
+
         output = '**Cortex Game Information**\n'
         if not self.assets.is_empty():
             output += '\n**Assets**\n'
@@ -607,17 +721,23 @@ class CortexGame:
         return output
 
 class Roller:
+    """Generates random die rolls and remembers the frequency of results."""
+
     def __init__(self):
         self.results = {}
         for size in DIE_SIZES:
             self.results[size] = [0] * size
 
     def roll(self, size):
+        """Roll a die of a given size and return the result."""
+
         face = random.SystemRandom().randrange(1, int(size) + 1)
         self.results[size][face - 1] += 1
         return face
 
     def output(self):
+        """Return a report of die roll frequencies."""
+
         total = 0
 
         frequency = ''
@@ -645,7 +765,10 @@ class Roller:
         return output
 
 class CortexPal(commands.Cog):
+    """This cog encapsulates the commands and state of the bot."""
+
     def __init__(self, bot):
+        """Initialize."""
         self.bot = bot
         self.games = []
         self.startup_time = datetime.utcnow()
@@ -653,9 +776,11 @@ class CortexPal(commands.Cog):
         self.roller = Roller()
 
     def update_command_time(self):
+        """Update the 'most recent user command' timestamp."""
         self.last_command_time = datetime.utcnow()
 
     def get_game_info(self, context):
+        """Match a server and channel to a Cortex game."""
         game_info = None
         game_key = [context.guild.id, context.message.channel.id]
         for existing_game in self.games:
@@ -668,6 +793,7 @@ class CortexPal(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+        """Intercepts any exceptions we haven't specifically caught elsewhere."""
         logging.error(error)
         if isinstance(error, commands.CommandNotFound):
             await ctx.send(UNKNOWN_COMMAND_ERROR)
